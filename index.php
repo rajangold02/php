d5d8faf6b32acdff8b4b2740e95bd46ff7e1beee
rajangold02 <rajangold02@gmail.com>
2019-02-11 13:15:32 +0530

    jenkins Build

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
   echo 'Hello World!';
   echo '<h1>PHP is so easy!</h1>';
-  echo 'changes for deployment';
-  echo 'checking deployment using GUI';
-  echo 'build using CLI failed';
+  echo 'Successfully builded with jenkins!!'
 ?>
